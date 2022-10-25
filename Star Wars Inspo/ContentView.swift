33d2fb6adfd56f7f7d527f54e87ec8217e929143
DaeheeCodes <DaeheeCodes@gmail.com>
2022-10-25 12:19:39 -0700

    Initial Commit

---
+++ Star Wars Inspo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Star Wars Inspo
+//
+//  Created by DHC on 10/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Star Wars Inspo/Star_Wars_InspoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Star_Wars_InspoApp.swift
+//  Star Wars Inspo
+//
+//  Created by DHC on 10/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Star_Wars_InspoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
